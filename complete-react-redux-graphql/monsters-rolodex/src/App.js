102c7b343bbb6d0319d80b79c9248e1129b1a386
ad0x99 <anhduc.dev99@gmail.com>
2023-01-02 14:21:39 +0700

    feat(react-basic):  component state, setState and callback

--- complete-react-redux-graphql/monsters-rolodex/src/App.js
+++ complete-react-redux-graphql/monsters-rolodex/src/App.js
@@ -4,12 +4,34 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      name: 'Thomas',
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello! Welcome to Monsters Rolodex</p>
+          <p>Hello {this.state.name}! Welcome to Monsters Rolodex</p>
+          <button
+            onClick={() => {
+              this.setState(
+                () => {
+                  return { name: 'Thomas Bui' };
+                },
+                () => {
+                  console.log(this.state);
+                }
+              );
+            }}
+          >
+            Change Name
+          </button>
         </header>
       </div>
     );

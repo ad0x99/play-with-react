26664e37fe6f8b33d63c65151fb20776f800be1d
ad0x99 <anhduc.dev99@gmail.com>
2023-01-02 16:08:40 +0700

    feat(react-basic): add searching user name

--- complete-react-redux-graphql/monsters-rolodex/src/App.js
+++ complete-react-redux-graphql/monsters-rolodex/src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: '',
     };
   }
 
@@ -15,23 +16,40 @@ class App extends Component {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) => {
-        this.setState(
-          () => {
-            return {
-              monsters: users,
-            };
-          },
-          () => {
-            console.log(this.state);
-          }
-        );
+        this.setState(() => {
+          return {
+            monsters: users,
+          };
+        });
       });
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLowerCase();
+
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search monsters"
+          onChange={onSearchChange}
+        />
+
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>

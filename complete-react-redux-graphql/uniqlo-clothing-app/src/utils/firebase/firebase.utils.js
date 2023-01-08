213e0c4ad58da71d107e3e73d1fdfb1dd5ab80b7
ad0x99 <anhduc.dev99@gmail.com>
2023-01-08 16:52:19 +0700

    feat(authentication): create user doc from auth

--- complete-react-redux-graphql/uniqlo-clothing-app/src/utils/firebase/firebase.utils.js
+++ complete-react-redux-graphql/uniqlo-clothing-app/src/utils/firebase/firebase.utils.js
@@ -1,23 +1,56 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
+// Firebase config
 const firebaseConfig = {
-  apiKey: ProcessingInstruction.env.REACT_APP_API_KEY,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+// Initialize firebase application
+initializeApp(firebaseConfig);
 
-const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
 
-const provider = new GoogleAuthProvider(app);
+// Config Google Authentication
+const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
   prompt: 'select_account',
 });
 
-const auth = getAuth();
+// Sign in with google
 const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
-export { auth, signInWithGooglePopup };
+// Create user document and save to firestore
+const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = await doc(db, 'users', userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      throw new Error(
+        'Error when creating user from authentication',
+        error.message
+      );
+    }
+  }
+
+  return userDocRef;
+};
+
+export { auth, signInWithGooglePopup, db, createUserDocumentFromAuth };

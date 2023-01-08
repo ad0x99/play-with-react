b02668e32e4c3221f1a1eb4047bf143e3cc1c18b
ad0x99 <anhduc.dev99@gmail.com>
2023-01-08 16:18:36 +0700

    feat(authentication): setup firebase authentication

--- complete-react-redux-graphql/uniqlo-clothing-app/src/App.js
+++ complete-react-redux-graphql/uniqlo-clothing-app/src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/Home/Home.component.jsx';
 import Navigation from './routes/Navigation/Navigation.component.jsx';
+import SignIn from './routes/SignIn/SignIn.component.jsx';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );

a77120958b8c94b689cc2848a6b9d22ba187f7d3
ad0x99 <anhduc.dev99@gmail.com>
2023-01-09 20:39:50 +0700

    feat(authentication): add Google sign in with redirect

--- complete-react-redux-graphql/uniqlo-clothing-app/src/routes/SignIn/SignIn.component.jsx
+++ complete-react-redux-graphql/uniqlo-clothing-app/src/routes/SignIn/SignIn.component.jsx
@@ -1,9 +1,26 @@
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
 import {
+  auth,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
+  signInWithGoogleRedirect,
 } from '../../utils/firebase/firebase.utils.js';
 
 const SignIn = () => {
+  useEffect(() => {
+    async function fetchRedirectResult() {
+      const response = await getRedirectResult(auth);
+
+      if (response) {
+        const { user } = response;
+        await createUserDocumentFromAuth(user);
+      }
+    }
+    fetchRedirectResult();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     await createUserDocumentFromAuth(user);
@@ -13,6 +30,9 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button>
     </div>
   );
 };

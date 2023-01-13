a070c26620115e31a34516e58fc56821c4cf175f
ad0x99 <anhduc.dev99@gmail.com>
2023-01-13 21:24:07 +0700

    feat(uniqlo): calculate total cart of checkout

--- complete-react-redux-graphql/uniqlo-clothing-app/src/routes/Checkout/Checkout.component.jsx
+++ complete-react-redux-graphql/uniqlo-clothing-app/src/routes/Checkout/Checkout.component.jsx
@@ -4,7 +4,7 @@ import { CartContext } from '../../context/cart.context';
 import './Checkout.styles.scss';
 
 const Checkout = () => {
-  const { cartItems } = useContext(CartContext);
+  const { cartItems, cartTotal } = useContext(CartContext);
 
   return (
     <div className="checkout-container">
@@ -35,7 +35,7 @@ const Checkout = () => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
 
-      <span className="total">Total: 0</span>
+      <span className="total">Total: ${cartTotal}</span>
     </div>
   );
 };

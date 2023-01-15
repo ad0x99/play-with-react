3a223b61f7c483ff302a18f0196deecd4aaa0bd9
ad0x99 <anhduc.dev99@gmail.com>
2023-01-15 23:06:22 +0700

    feat(uniqlo): migrate home category data to firestore

--- complete-react-redux-graphql/uniqlo-clothing-app/src/components/Directory/Directory.component.jsx
+++ complete-react-redux-graphql/uniqlo-clothing-app/src/components/Directory/Directory.component.jsx
@@ -1,12 +1,22 @@
+import { useContext, useEffect, useState } from 'react';
 import DirectoryItem from '../DirectoryItem/DirectoryItem.component';
 import { DirectoryContainer } from './Directory.styles.jsx';
+import { CategoriesContext } from '../../context/category.context.js';
 
-const Directory = ({ categories }) => {
+const Directory = () => {
+  const { homeCategoriesMap } = useContext(CategoriesContext);
+  const [homeCategories, setHomeCategories] = useState([]);
+
+  useEffect(() => {
+    setHomeCategories(homeCategoriesMap);
+  }, [homeCategoriesMap]);
+
   return (
     <DirectoryContainer>
-      {categories.map((category) => (
-        <DirectoryItem key={category.id} category={category} />
-      ))}
+      {homeCategories.length &&
+        homeCategories.map((category) => (
+          <DirectoryItem key={category.id} category={category} />
+        ))}
     </DirectoryContainer>
   );
 };
--- complete-react-redux-graphql/uniqlo-clothing-app/src/context/category.context.js
+++ complete-react-redux-graphql/uniqlo-clothing-app/src/context/category.context.js
@@ -1,18 +1,23 @@
 import { useState, useEffect } from 'react';
 import { createContext } from 'react';
-import { getCategoriesAndDocuments } from '../utils/firebase/product.utils';
+import {
+  getCategoriesAndDocuments,
+  getHomeCategoriesAndDocuments,
+} from '../utils/firebase/product.utils';
 
 const CategoriesContext = createContext({
   categoriesMap: {},
+  homeCategoriesMap: [],
 });
 
 const CategoriesProvider = ({ children }) => {
   const [categoriesMap, setCategoriesMap] = useState({});
+  const [homeCategoriesMap, setHomeCategoriesMap] = useState({});
 
   useEffect(() => {
     const getCategoriesMap = async () => {
       const data = await getCategoriesAndDocuments(
-        process.env.REACT_APP_FIREBASE_COLLECTION_NAME
+        process.env.REACT_APP_FIREBASE_CATEGORIES_NAME
       );
       setCategoriesMap(data);
     };
@@ -20,15 +25,34 @@ const CategoriesProvider = ({ children }) => {
     getCategoriesMap();
   }, []);
 
+  useEffect(() => {
+    const getHomeCategoriesMap = async () => {
+      const data = await getHomeCategoriesAndDocuments(
+        process.env.REACT_APP_FIREBASE_HOME_CATEGORIES_NAME
+      );
+      setHomeCategoriesMap(data);
+    };
+
+    getHomeCategoriesMap();
+  }, []);
+
   // Migrate products data to firestore
   // useEffect(() => {
   //   addCollectionAndDocuments(
-  //     process.env.REACT_APP_FIREBASE_COLLECTION_NAME,
+  //     process.env.REACT_APP_FIREBASE_HOME_CATEGORIES_NAME,
+  //     categories
+  //   );
+  // }, []);
+
+  // Migrate products data to firestore
+  // useEffect(() => {
+  //   addCollectionAndDocuments(
+  //     process.env.REACT_APP_FIREBASE_CATEGORIES_NAME,
   //     PRODUCTS
   //   );
-  // }, [productsMigration.length]);
+  // }, []);
 
-  const value = { categoriesMap };
+  const value = { categoriesMap, homeCategoriesMap };
 
   return (
     <CategoriesContext.Provider value={value}>

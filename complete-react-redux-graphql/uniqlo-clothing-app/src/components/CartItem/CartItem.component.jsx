d208945e2f4178fafd1e8087b9795cd5b4f7cac3
ad0x99 <anhduc.dev99@gmail.com>
2023-01-15 21:45:26 +0700

    feat(uniqlo): apply styled component for cart item

--- complete-react-redux-graphql/uniqlo-clothing-app/src/components/CartItem/CartItem.component.jsx
+++ complete-react-redux-graphql/uniqlo-clothing-app/src/components/CartItem/CartItem.component.jsx
@@ -1,19 +1,19 @@
-import './CartItem.styles.scss';
+import { CartItemContainer, Image, ItemDetails } from './CartItem.styles.jsx';
 
 const CartItem = ({ cartItem }) => {
   const { name, quantity, imageUrl, price } = cartItem;
 
   return (
-    <div className="cart-item-container">
-      <img src={imageUrl} alt={`${name}`} />
+    <CartItemContainer>
+      <Image src={imageUrl} alt={`${name}`} />
 
-      <div className="item-details">
+      <ItemDetails>
         <h2 className="name">{name}</h2>
         <span className="price">
           {quantity} x ${price}
         </span>
-      </div>
-    </div>
+      </ItemDetails>
+    </CartItemContainer>
   );
 };
 

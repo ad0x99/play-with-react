f73aa34a1277d198693b59c2cce34aa77c56ae24
ad0x99 <anhduc.dev99@gmail.com>
2023-01-15 14:53:12 +0700

    feat(uniqlo): add category component

--- complete-react-redux-graphql/uniqlo-clothing-app/src/routes/Shop/Shop.component.jsx
+++ complete-react-redux-graphql/uniqlo-clothing-app/src/routes/Shop/Shop.component.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import CategoriesPreview from '../CategoriesPreview/CategoriesPreview.component';
+import Category from '../Category/Category.component';
 import './Shop.styles.scss';
 
 const Shop = () => {
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
     </Routes>
   );
 };

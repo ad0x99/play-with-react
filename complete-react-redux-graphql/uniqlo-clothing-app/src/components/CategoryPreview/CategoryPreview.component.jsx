87220edf2df54177c933aed31b5d26afbf77aaba
ad0x99 <anhduc.dev99@gmail.com>
2023-01-15 21:48:30 +0700

    feat(uniqlo): apply styled component for category preview

--- complete-react-redux-graphql/uniqlo-clothing-app/src/components/CategoryPreview/CategoryPreview.component.jsx
+++ complete-react-redux-graphql/uniqlo-clothing-app/src/components/CategoryPreview/CategoryPreview.component.jsx
@@ -1,24 +1,25 @@
-import { Link } from 'react-router-dom';
 import ProductCard from '../ProductCard/ProductCard.component';
-import './CategoryPreview.styles.scss';
+import {
+  CategoryPreviewContainer,
+  Preview,
+  Title,
+} from './CategoryPreview.styles.jsx';
 
 const CategoryPreview = ({ title, products }) => {
   return (
-    <div className="category-preview-container">
+    <CategoryPreviewContainer>
       <h2>
-        <Link className="title" to={title}>
-          {title.toUpperCase()}
-        </Link>
+        <Title to={title}>{title.toUpperCase()}</Title>
       </h2>
 
-      <div className="preview">
+      <Preview>
         {products
           .filter((_, idx) => idx < 4)
           .map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-      </div>
-    </div>
+      </Preview>
+    </CategoryPreviewContainer>
   );
 };
 

441df7aedbc5a254f96ff8ad9349030938cf92dc
ad0x99 <anhduc.dev99@gmail.com>
2023-01-15 15:19:53 +0700

    feat(uniqlo): navigation for category

--- complete-react-redux-graphql/uniqlo-clothing-app/src/components/ProductCard/ProductCard.component.jsx
+++ complete-react-redux-graphql/uniqlo-clothing-app/src/components/ProductCard/ProductCard.component.jsx
@@ -15,7 +15,7 @@ const ProductCard = ({ product }) => {
 
       <div className="footer">
         <span className="name">{name}</span>
-        <span className="price">{price}</span>
+        <span className="price">${price}</span>
       </div>
 
       <Button buttonType="inverted" onClick={addProductToCart}>

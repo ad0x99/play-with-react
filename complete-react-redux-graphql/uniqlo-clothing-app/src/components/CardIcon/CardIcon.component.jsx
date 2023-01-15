abddd9b71db6ef1a174a91c60b91808bed7eba3c
ad0x99 <anhduc.dev99@gmail.com>
2023-01-15 20:56:37 +0700

    feat(uniqlo): apply styled component for cart icon

--- complete-react-redux-graphql/uniqlo-clothing-app/src/components/CardIcon/CardIcon.component.jsx
+++ complete-react-redux-graphql/uniqlo-clothing-app/src/components/CardIcon/CardIcon.component.jsx
@@ -1,8 +1,6 @@
 import { useContext } from 'react';
-import { ReactComponent as ShoppingIcon } from '../../assets/cart.svg';
 import { CartContext } from '../../context/cart.context';
-
-import './CardIcon.styles.scss';
+import { CartIconContainer, ItemCount, ShoppingIcon } from './CardIcon.styles';
 
 const CardIcon = () => {
   const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
@@ -10,10 +8,10 @@ const CardIcon = () => {
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
-    <div className="cart-icon-container" onClick={toggleIsCartOpen}>
-      <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">{cartCount}</span>
-    </div>
+    <CartIconContainer onClick={toggleIsCartOpen}>
+      <ShoppingIcon />
+      <ItemCount className="item-count">{cartCount}</ItemCount>
+    </CartIconContainer>
   );
 };
 

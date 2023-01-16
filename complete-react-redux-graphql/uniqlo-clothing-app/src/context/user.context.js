fc48d1a7bf95f49bf72b05a6a1626f8dcea0599a
ad0x99 <anhduc.dev99@gmail.com>
2023-01-16 21:03:39 +0700

    feat(uniqlo): add user reducer

--- complete-react-redux-graphql/uniqlo-clothing-app/src/context/user.context.js
+++ complete-react-redux-graphql/uniqlo-clothing-app/src/context/user.context.js
@@ -1,4 +1,5 @@
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
+import { USER_ACTION_TYPES } from '../Constants/userAction.const';
 import {
   createUserDocumentFromAuth,
   onAuthStateChangedListener,
@@ -9,8 +10,33 @@ const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  const { currentUser } = state;
+
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+  };
+
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {

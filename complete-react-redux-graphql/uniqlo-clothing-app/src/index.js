d92343fa01f7d6bc939ebf3215dc2d818997706e
ad0x99 <anhduc.dev99@gmail.com>
2023-01-17 21:45:47 +0700

    feat(uniqlo-redux): setup root reducer and store

--- complete-react-redux-graphql/uniqlo-clothing-app/src/index.js
+++ complete-react-redux-graphql/uniqlo-clothing-app/src/index.js
@@ -2,25 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
+import { Provider } from 'react-redux';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { UserProvider } from './context/user.context';
 import { CategoriesProvider } from './context/category.context';
 import { CartProvider } from './context/cart.context';
+import store from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
-
-reportWebVitals();

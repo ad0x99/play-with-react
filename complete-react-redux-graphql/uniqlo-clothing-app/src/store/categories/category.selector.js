7da7f19e98ed1234b92bf08df8a63ac8890fb732
ad0x99 <anhduc.dev99@gmail.com>
2023-01-18 20:57:21 +0700

    feat(uniqlo-redux): using memoized selector to optimize categories selector

--- complete-react-redux-graphql/uniqlo-clothing-app/src/store/categories/category.selector.js
+++ complete-react-redux-graphql/uniqlo-clothing-app/src/store/categories/category.selector.js
@@ -1,15 +1,36 @@
-const selectCategoriesArray = (state) => {
-  const categories = state.categories.categoriesArray.reduce(
-    (acc, { title, items }) => {
-      acc[title.toLowerCase()] = items;
-      return acc;
-    },
-    {}
-  );
-  return categories;
+import { createSelector } from 'reselect';
+
+const selectCategoryReducer = (state) => {
+  return state.categories;
 };
 
-const selectHomeCategoriesArray = (state) =>
-  state.categories.homeCategoriesArray;
+/**
+ * Create a memoized selector to cache categories state.
+ */
+const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => {
+    return categoriesSlice;
+  }
+);
 
-export { selectCategoriesArray, selectHomeCategoriesArray };
+/**
+ * As long as the categories array does not change, do not re-run reduce method.
+ * Then just return the previous values.
+ * This will prevent to running unnecessary call to get the data
+ */
+const selectCategoriesMap = createSelector([selectCategories], (categories) => {
+  return categories.categoriesArray.reduce((acc, { title, items }) => {
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {});
+});
+
+const selectHomeCategoriesArray = createSelector(
+  [selectCategories],
+  (categories) => {
+    return categories.homeCategoriesArray;
+  }
+);
+
+export { selectCategoriesMap, selectHomeCategoriesArray };

368f74a68edd35703df1c67ab0522a4f7739a15b
ad0x99 <anhduc.dev99@gmail.com>
2023-01-18 16:05:18 +0700

    feat(uniqlo-redux): custom logger middleware for testing

--- complete-react-redux-graphql/uniqlo-clothing-app/src/store/store.js
+++ complete-react-redux-graphql/uniqlo-clothing-app/src/store/store.js
@@ -3,6 +3,21 @@ import logger from 'redux-logger';
 
 import rootReducer from './rootReducer';
 
+// Custom logger middleware for testing purposes
+// const loggerMiddleware = (store) => (next) => (action) => {
+//   if (!action.type) {
+//     return next(action);
+//   }
+
+//   console.log('type', action.type);
+//   console.log('payload', action.payload);
+//   console.log('currentState', store.getState());
+
+//   next(action);
+
+//   console.log('next state', store.getState());
+// };
+
 const middlewares = [logger];
 
 const composedEnhancers = compose(applyMiddleware(...middlewares));

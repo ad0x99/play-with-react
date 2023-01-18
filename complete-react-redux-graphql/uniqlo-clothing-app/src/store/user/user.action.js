e0697961db4452af243f9ffde6acb32744269f94
ad0x99 <anhduc.dev99@gmail.com>
2023-01-18 15:14:06 +0700

    feat(uniqlo): remove unnecessary files

--- complete-react-redux-graphql/uniqlo-clothing-app/src/store/user/user.action.js
+++ complete-react-redux-graphql/uniqlo-clothing-app/src/store/user/user.action.js
@@ -1,5 +1,5 @@
 import { createAction } from '../../utils/reducer/reducer.utils';
-import { USER_ACTION_TYPES } from '../../Constants/userAction.const';
+import { USER_ACTION_TYPES } from './user.types';
 
 const setCurrentUser = (user) => {
   return createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user);

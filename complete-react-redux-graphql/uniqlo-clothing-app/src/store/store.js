da8ddb47fb16fcb4265fcaee7d8fde6d3f4bcc84
ad0x99 <anhduc.dev99@gmail.com>
2023-01-20 14:49:41 +0700

    feat(uniqlo-redux-thunk): only persist cart data

--- complete-react-redux-graphql/uniqlo-clothing-app/src/store/store.js
+++ complete-react-redux-graphql/uniqlo-clothing-app/src/store/store.js
@@ -9,7 +9,7 @@ import rootReducer from './rootReducer';
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['user'],
+  whitelist: ['cart'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);

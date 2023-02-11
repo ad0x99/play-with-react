57c607becf68ee4f1e502272fbc7816f0061d3b1
Duc (Thomas) <50711511+ad0x99@users.noreply.github.com>
2023-02-11 16:11:02 +0700

    feat(uniqlo): add payment form and payment feature using stripe serverless function (#24)
    * feat(uniqlo-payment): add stripe config
    
    * feat(uniqlo-payment): add PaymentForm and update README
    
    * feat(uniqlo-payment): add CreatePaymentIntent serverless function
    
    * feat(uniqlo-payment): integrate payment for checkout form
    
    * feat(uniqlo-payment): add loading spinner for payment button

--- complete-react-redux-graphql/uniqlo-clothing-app/README.md
+++ complete-react-redux-graphql/uniqlo-clothing-app/README.md
@@ -11,6 +11,7 @@ yarn install
 - In this project, I'm using [Infisical](https://infisical.com/) to manage my environment variables. But you can use `.env` file as usual.
 
 - To get your environment value, create your firebase project, then create a firebase app. Then you can find the configuration
+- To get the stripe payment service key, you have to sign up for stripe account, then you can get the test mode public key and private key
 - Add your own configuration to env variables as below. (Don't replace the existing ones)
 
 ```JSON
@@ -23,6 +24,8 @@ REACT_APP_FIREBASE_HOME_CATEGORIES_NAME=homeCategories
 REACT_APP_FIREBASE_MESSAGING_SENDER_ID=
 REACT_APP_FIREBASE_PROJECT_ID=
 REACT_APP_FIREBASE_STORAGE_BUCKET=
+REACT_APP_STRIPE_PUBLIC_KEY=
+REACT_APP_STRIPE_SECRET_KEY=
 ```
 
 3. Start the application
---
+++ complete-react-redux-graphql/uniqlo-clothing-app/src/components/PaymentForm/PaymentForm.component.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+
+import {
+  FormContainer,
+  PaymentButton,
+  PaymentFormContainer,
+} from './PaymentForm.styles.jsx';
+import { selectCartTotal } from '../../store/cart/cart.selector.js';
+import { selectCurrentUser } from '../../store/user/user.selector.js';
+
+const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const amount = useSelector(selectCartTotal);
+  const currentUser = useSelector(selectCurrentUser);
+  const [isProcessingPayment, setIsProcessingPayment] = useState(false);
+
+  const paymentHandler = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) return;
+
+    setIsProcessingPayment(true);
+
+    const response = await fetch('/.netlify/functions/create-payment-intent', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ amount }),
+    }).then((res) => res.json());
+
+    const {
+      paymentIntent: { client_secret },
+    } = response;
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name:
+            currentUser && currentUser.displayName !== ' '
+              ? currentUser.displayName
+              : 'Guest',
+        },
+      },
+    });
+
+    setIsProcessingPayment(false);
+
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === 'succeeded') {
+        alert('Payment successful!');
+      }
+    }
+  };
+
+  return (
+    <PaymentFormContainer>
+      <FormContainer onSubmit={paymentHandler}>
+        <h2>Credit Card Payment: </h2>
+        <CardElement />
+        <PaymentButton isLoading={isProcessingPayment}>Pay Now</PaymentButton>
+      </FormContainer>
+    </PaymentFormContainer>
+  );
+};
+
+export default PaymentForm;

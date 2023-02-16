436aa66fdf8cdfa7eca4c0f1cdb480018119553a
ad0x99 <anhduc.dev99@gmail.com>
2023-02-16 21:30:33 +0700

    feat(uniqlo-ts): apply ts for root saga

--- complete-react-redux-graphql/uniqlo-clothing-app/src/store/rootSaga.ts
+++ complete-react-redux-graphql/uniqlo-clothing-app/src/store/rootSaga.ts
@@ -1,9 +1,10 @@
-import { all, call } from 'redux-saga/effects';
+import { all, call } from 'typed-redux-saga/macro';
+
 import { categoriesSaga } from './categories/category.saga';
 import { userSagas } from './user/user.saga';
 
 function* rootSaga() {
-  yield all([call(categoriesSaga), call(userSagas)]);
+  yield* all([call(categoriesSaga), call(userSagas)]);
 }
 
 export { rootSaga };

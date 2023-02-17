e3a327ca2d20cd6b8b009e4eb623cf3213b1b14a
ad0x99 <anhduc.dev99@gmail.com>
2023-02-17 20:56:30 +0700

    feat(uniqlo-ts): apply ts for svg logo

--- complete-react-redux-graphql/uniqlo-clothing-app/src/routes/Navigation/Navigation.component.tsx
+++ complete-react-redux-graphql/uniqlo-clothing-app/src/routes/Navigation/Navigation.component.tsx
@@ -10,7 +10,7 @@ import {
   LogoContainer,
   NavLinkContainer,
   NavLink,
-} from './Navigation.styles.jsx';
+} from './Navigation.styles.js';
 import { selectCurrentUser } from '../../store/user/user.selector';
 import { selectIsCartOpen } from '../../store/cart/cart.selector';
 import { signOutStart } from '../../store/user/user.action';

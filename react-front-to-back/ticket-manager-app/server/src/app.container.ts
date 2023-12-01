c87026e60fb7458b9cb22f07104d0f137309d7b6
ad0x99 <50711511+ad0x99@users.noreply.github.com>
2023-12-01 16:13:39 +0700

    feat(ticket-manager-app-server): update .env.example and remove unnecessary folders

--- react-front-to-back/ticket-manager-app/server/src/app.container.ts
+++ react-front-to-back/ticket-manager-app/server/src/app.container.ts
@@ -1,14 +1,12 @@
-import { Container } from "inversify";
 import { AppContainer } from "@expressots/core";
 import { AppModule } from "@useCases/app/app.module";
-import { UserModule } from "@useCases/user/user.module";
 import { AuthenticationModule } from "@useCases/authentication/auth.module";
+import { Container } from "inversify";
 
 const appContainer = new AppContainer();
 
 export const container: Container = appContainer.create([
     // Add your modules here
     AppModule,
-    UserModule,
     AuthenticationModule,
 ]);
